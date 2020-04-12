ab72bf9c154d44551ab7e886accae6779f42b4ec
bistudio <basswrks@hotmail.com>
2020-04-12 16:45:42 +0100

    updated with file created datetime

--- portal.py
+++ portal.py
@@ -6,14 +6,6 @@ import logging as log
 from functools import lru_cache
 from tkinter import *
 
-# file_ico_copy = shu.copy("./file_1.ico", "./source")
-# with open("./source/text_file.txt", "a") as f:
-#     for i in range(6, 10):
-#         f.write(f'This is line {i}\n')
-
-# create log file
-
-
 # provide path to source folder
 
 source_folder = "./source"
@@ -21,7 +13,6 @@ source_folder = "./source"
 # get list of files in source folder
 
 file_list = os.listdir(source_folder)
-# file_list.remove(list_of_files[0])
 list_of_files = []
 
 # remove unwanted process files
@@ -36,16 +27,16 @@ for list in file_list:
 # get the year and month of date file was modified
 
 for file in list_of_files:
-    st = os.stat(source_folder+"/"+file)
-    file_time = dt.datetime.fromtimestamp(st.st_ctime)
+    file_time_seconds = os.path.getctime(source_folder+"/"+file)
+    file_time = dt.datetime.fromtimestamp(file_time_seconds)
     file_year = file_time.strftime("%Y")
     file_month = file_time.strftime("%B")
     file_year_month = file_month + " " + file_year
     file_year_dir = source_folder + "/" + file_year
     file_month_year_dir = source_folder + "/" + file_year + "/" + file_year_month
     try:
-        os.mkdir(file_year_dir)
-        os.mkdir(file_month_year_dir)
+        os.mkdir(file_year_dir)  # created year of file
+        os.mkdir(file_month_year_dir)  # created month of file
     except FileExistsError:
         pass
     try:
@@ -61,9 +52,3 @@ for file in list_of_files:
         pass
 
     print(f'{file}, {file_year}, {file_year_month}, {source_folder+"/"+file}, {file_month_year_dir}')
-
-# log activities
-
-log.basicConfig(filename="./portal.log", level=log.CRITICAL)
-log.basicConfig(filename="./portal.log", level=log.WARNING)
-log.basicConfig(filename="./portal.log", level=log.INFO)
