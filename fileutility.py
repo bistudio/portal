0cee72d0a9a1c6206c0768970659ee93156d8bd5
bistudio <basswrks@hotmail.com>
2020-05-09 08:18:14 +0100

    updated to change cancel and submit button state dynamically depending on entry in source directory input box

--- fileutility.py
+++ fileutility.py
@@ -1,72 +1,70 @@
-from tkinter import *
-
-window = Tk()
-window.geometry("300x600")
-window.resizable(width=False, height=False)
-window.title("File Utility")
-window.iconbitmap("File-Explorer.ico")
-
-padding_1 = Label(text="")
-padding_1.pack()
-
-top_Label = Label(text="File Utility", font=20)
-top_Label.pack()
-
-padding_2 = Label(text="")
-padding_2.pack()
-padding_3 = Label(text="")
-padding_3.pack()
-
-source_dir_label = Label(window, text="Source Directory")
-source_dir_label.pack()
-source_dir_entry = Entry(window, width=40)
-source_dir_entry.pack()
-padding_4 = Label(text="")
-padding_4.pack()
-
-dest_dir_label = Label(window, text="Destination Directory")
-dest_dir_label.pack()
-dest_dir_entry = Entry(window, width=40)
-dest_dir_entry.pack()
-padding_5 = Label(text="")
-padding_5.pack()
-
-file_operation_Label = Label(text="File Operation", font=14)
-file_operation_Label.pack()
-padding_6 = Label(text="")
-padding_6.pack()
-
-file_operation_frame = Frame(window, width=60, height=60)
-file_operation_frame.pack()
-
-# file operation function
+import tkinter as tk
 
 
-v = IntVar()
+if __name__ == '__main__':
+    window = tk.Tk()
+    window.geometry("300x450")
+    window.resizable(width=False, height=False)
+    window.title("Portal")
+    window.iconbitmap("File-Explorer.ico")
 
-R1 = Radiobutton(file_operation_frame, value="rename", text="Rename Files", variable=v)
-R1.pack(anchor=W)
-R2 = Radiobutton(file_operation_frame, value="move", text="Move Files", variable=v)
-R2.pack(anchor=W)
-R3 = Radiobutton(file_operation_frame, value="archive", text="Archive Files", variable=v)
-R3.pack(anchor=W)
+    padding_1 = tk.Label(text="")
+    padding_1.pack()
 
-padding_7 = Label(text="")
-padding_7.pack()
-padding_8 = Label(text="")
-padding_8.pack()
+    top_Label = tk.Label(text="Portal File Utility", font=20)
+    top_Label.pack()
 
-actionframe = Frame(window, width=80, height=80)
-actionframe.pack()
+    padding_2 = tk.Label(text="")
+    padding_2.pack()
+    padding_3 = tk.Label(text="")
+    padding_3.pack()
 
-cancelbtn = Button(actionframe, text="Cancel", width=10, padx=10, pady=5, state=DISABLED)
-cancelbtn.pack(side=LEFT)
-submitbtn = Button(actionframe, text="Submit", width=10, padx=10, pady=5, state=DISABLED)
-submitbtn.pack(side=RIGHT)
+    source_dir_label = tk.Label(window, text="Source Directory")
+    source_dir_label.pack()
+    source_dir_entry = tk.Entry(window, width=40)
+    source_dir_entry.pack()
+    padding_4 = tk.Label(text="")
+    padding_4.pack()
 
 
+    def clear_source_entry():
+        source_dir_entry.delete(0, 'end')
 
-window.mainloop()
 
-if __name__ == "main":
-    window.run()
+    file_operation_Label = tk.Label(text="File Operation Progress", font=12)
+    file_operation_Label.pack()
+    padding_6 = tk.Label(text="")
+    padding_6.pack()
+
+    file_operation_frame = tk.Frame(window, width=60, height=60)
+    file_operation_frame.pack()
+
+    actionframe = tk.Frame(window, width=80, height=80)
+    actionframe.pack()
+
+    cancelbtn = tk.Button(actionframe, text="Cancel", width=10, padx=10, pady=5, state='disabled',
+                          command=clear_source_entry)
+    cancelbtn.pack(side='left')
+    submitbtn = tk.Button(actionframe, text="Submit", width=10, padx=10, pady=5, state='disabled')
+    submitbtn.pack(side='right')
+
+
+    def toggle_state(*_):
+        if source_dir_entry.var.get():
+            cancelbtn['state'] = 'normal'
+            submitbtn['state'] = 'normal'
+
+        else:
+            cancelbtn['state'] = 'disabled'
+            submitbtn['state'] = 'disabled'
+
+
+    source_dir_entry.var = tk.StringVar()
+    source_dir_entry['textvariable'] = source_dir_entry.var
+    source_dir_entry.var.trace_add('write', toggle_state)
+
+    padding_9 = tk.Label(text="")
+    padding_9.pack()
+    padding_10 = tk.Label(text="")
+    padding_10.pack()
+    window.mainloop()
